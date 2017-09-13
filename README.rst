5ea87aff1fa94757c72452ca01832d66577f161d
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-09-13 15:36:17 +0200

    Set up package skeleton

---
+++ README.rst
@@ -0,0 +1,15 @@
+=========================
+celery_longterm_scheduler
+=========================
+
+
+
+
+Run tests
+=========
+
+Using `tox`_ and `py.test`_. Maybe install ``tox`` (e.g. via ``pip install tox``)
+and then simply run ``tox``.
+
+.. _`tox`: http://tox.readthedocs.io/
+.. _`py.test`: http://pytest.org/
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='celery_longterm_scheduler',
+    version='1.0.0.dev0',
+    author='Zeit Online',
+    author_email='zon-backend@zeit.de',
+    url='http://www.zeit.de/',
+    description="Schedules celery tasks to run in the potentially far future",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='BSD',
+    install_requires=[
+        'celery',
+        'redis',
+        'setuptools',
+    ],
+    extras_require={'test': [
+        'mock',
+    ]},
+    entry_points={
+        'celery.commands': [
+            'longterm_scheduler = celery_longterm_scheduler.scheduler:main',
+        ]
+    }
+)
