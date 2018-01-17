a0e5c41a2b5c71c5efa52c0007635f78833c4a10
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-01-17 13:39:31 +0100

    Back to development: 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='celery_longterm_scheduler',
-    version='1.0.1',
+    version='1.1.0.dev0',
     author='Zeit Online',
     author_email='zon-backend@zeit.de',
     url='https://github.com/zeitonline/celery_longterm_scheduler',
